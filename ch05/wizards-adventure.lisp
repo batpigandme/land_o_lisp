e8793e31247395d0bd8157b0327db5fdf6d808d4
Mara Averick <maraaverick@gmail.com>
2018-08-22 18:36:31 -0400

    🔭 Find visible objects

--- ch05/wizards-adventure.lisp
+++ ch05/wizards-adventure.lisp
@@ -24,3 +24,32 @@
     `(there is a ,(caddr edge) going ,(cadr edge) from here.))
 
 (describe-path '(garden west door))
+
+;; describe paths plural
+(defun describe-paths (location edges)
+    (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(describe-paths 'living-room *edges*)
+
+;; finding the relevant edges
+(cdr (assoc 'living-room *edges*))
+
+;; converting the edges to descriptions
+(mapcar #'describe-path '((GARDEN WEST DOOR) (ATTIC UPSTAIRS LADDER)))
+
+;; listing visible objects
+(defparameter *objects* '(whiskey bucket frog chain))
+
+;; add object-location alist
+(defparameter *object-locations* '((whiskey living-room)
+                                    (bucket living-room)
+                                    (chain garden)
+                                    (frog garden)))
+
+;; function listing objects at a location
+(defun objects-at (loc objs obj-locs)
+    (labels ((at-loc-p (obj)
+            (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
+
+(objects-at 'living-room *objects* *object-locations*)
