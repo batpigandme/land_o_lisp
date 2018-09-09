32e68ac58629778c6aa1eed4e168b318df96fdeb
Mara Averick <maraaverick@gmail.com>
2018-09-09 10:23:52 -0400

    Printing values

--- ch06/printing-to-the-screen.lisp
+++ ch06/printing-to-the-screen.lisp
@@ -11,3 +11,21 @@
         (prin1 "is")
         (prin1 "a")
         (prin1 "test"))
+
+;; read and print a number
+(defun add-five ()
+        (print "please enter a number:")
+        (let ((num (read)))
+                (print "When I add five I get")
+                (print (+ num 5))))
+(add-five)
+
+;; printing values
+(print '3)      ; an integer
+(print '3.4)    ; a float
+(print 'foo)    ; a symbol
+(print '"foo")  ; a string
+(print '#\a)    ; a character
+
+;; add vertical pipe for case sensitivity
+(print '|CaseSensitiveSymbol|)
